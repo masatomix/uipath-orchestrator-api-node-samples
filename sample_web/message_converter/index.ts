8ba3ded1940612c8fbf1d5969d41e3a4e4ecb5e4
Masatomi KINO <masatomix@ki-no.org>
2020-09-22 02:41:24 +0900

    CSVの行列を入れ替えたのでコード修正。

--- sample_web/message_converter/index.ts
+++ sample_web/message_converter/index.ts
@@ -1,19 +1,46 @@
 const csv = require('csvtojson')
 
+// usage
+// % pwd
+// /xxxxx/uipath-orchestrator-api-node-samples/sample_web
+// % npx ts-node ./message_converter/index.ts > ./src/lang.json
+
 function main() {
-  csv()
-    .fromFile('lang.csv')
-    .then((results: any[]) => {
-      const json = results.reduce((accumulator, row) => {
-        const key = row['key']
-        delete row['key']
+  csv({ noheader: true })
+    .fromFile('messages.csv')
+    .then((matrix: string[][]) => {
+      // 行列を入れ替え
+      const newResults: string[][] = transpose(matrix)
+      // 1行目はヘッダとしてshiftで取り出し、それ以外をデータ部とする
+      const headings: string[] = newResults.shift()
+
+      // rowとしてくる行データを使って、headingsのおなじindex番号をプロパティとして、obj を構成する
+      const json = newResults.reduce((accumulator, row: string[]) => {
+        const obj = {}
+        for (let index = 0; index < row.length; index++) {
+          obj[headings[index]] = row[index]
+        }
+
+        const key = obj['key']
+        delete obj['key'] // keyプロパティはデータとしては不要
+
         accumulator[key] = {
-          message: row,
+          message: obj,
         }
         return accumulator
       }, {})
       console.log(JSON.stringify(json))
     })
+}
+
+function transpose(matrix: string[][]): string[][] {
+  return Object.keys(matrix[0]).map((column) => matrix.map((row) => row[column]))
+  // const keys: string[] = Object.keys(matrix[0])
+  // // console.log(matrix[0])
+  // // 列方向の処理
+  // return keys.map((column) => {
+  //    return matrix.map((row) => row[column])
+  // })
 }
 
 if (!module.parent) {
