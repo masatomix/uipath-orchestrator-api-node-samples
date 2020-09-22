4072b27827a3f455ddf155fa6a2ae54a7beef8d6
Masatomi KINO <masatomix@ki-no.org>
2020-09-23 00:42:27 +0900

    ヘッダにEnterprise/Communityを表示 フッタ文字を小さく。

--- sample_web/src/components/OcFooter.vue
+++ sample_web/src/components/OcFooter.vue
@@ -7,7 +7,10 @@
     <button v-on:click="changeLocale('en')" style="margin: 5px">
       <flag iso="us" />
     </button>
-    <div>&copy; Masatomi KINO. {{ new Date().getFullYear() }} {{ urlStr }}</div>
+    <div>
+      &copy; Masatomi KINO.{{ new Date().getFullYear() }}
+      <span style="font-size: 0.8em">{{ urlStr }}</span>
+    </div>
   </v-footer>
 </template>
 
